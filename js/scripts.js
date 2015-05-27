3d5db937ec79de7c33199bacfb2ad78865fd1cb6
Erin Collins & Kim Dang <student@epicodus.com>
2015-05-27 16:54:52 -0700

    Add webapp pages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 var primeSift = function(number) {
 
-  var newArray = [];
-  var loopArray = [];
-  for(var i = 2; i <= number; ++i) {
+  var newArray = [2];
+
+  for(var i = 3; i <= number; i+=2) {
     newArray.push(i);
   }
 
   for(var i = 2; i <= number; ++i) {
     newArray = findPrime(i, newArray);
-
   }
-
 
   return newArray;
 };
@@ -26,33 +24,16 @@ var findPrime = function(prime, newArray) {
 };
 
 
-// if (number % 2 !== 0 || number === 2){
-//   if (number % 3 !== 0 || number === 3){
+$(document).ready(function(){
+  $("form#inputForm").submit(function(event){
+    var number = $("input#number").val();
+    start = performance.now();
+    var result = primeSift(number);
+    end = performance.now();
+    console.log(end - start)
+    $("#primes").text(result);
+    $("#results").show();
 
-// var findPrime = function(array) {
-//   array.forEach(function(number){
-//     prime = 2; prime <= number; ++prime){
-//       if (number % prime !== 0){
-//         primeArray.push(number);
-//       }
-//     }
-//   });
-//   return findPrime(primeArray);
-// }
-
-
-// for(var i = 3; i <= number; ++i){
-//   if (i % prime !== 0)
-//     primeArray.push(i);
-//   }
-// }
-// return primeArray;
-
-//   var start = 2
-//   numberArray.forEach(function(number) {
-
-//       ++start;
-//     }
-//   });
-//   return primeArray;
-// }
+    event.preventDefault();
+  });
+});
